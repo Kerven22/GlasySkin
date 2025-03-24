bd3dcda8a9c6712e668b95cd3f10436d3074e6d9
Kerven Jumayev <kerwen.jumazew@gmail.com>
2025-03-24 19:03:13 +0300

    some change in Repository.Contract and some change in Repositories

---
+++ Repositories/RepositoryManager.cs
@@ -0,0 +1,48 @@
+﻿using Repositories.DataBaseContext;
+using Repository.Contract;
+using Repository.Contract.Abstractions;
+
+namespace Repositories
+{
+    public sealed class RepositoryManager : IRepositoryManager
+    {
+        private readonly RepositoryContext _repositoryContext;
+        private readonly Lazy<ITypeRepository> _typeRepository;
+        private readonly Lazy<IProductRepository> _productRepository;
+        private readonly Lazy<ICommentRepository> _commentRepository;
+        private readonly Lazy<IUserRepository> _userRepository;
+        private readonly Lazy<IBasketRepository> _basketRepository; 
+
+
+        public RepositoryManager(RepositoryContext repositoryContext)
+        {
+            _repositoryContext = repositoryContext;
+
+            _typeRepository = new Lazy<ITypeRepository>(() => new TypeRepository(_repositoryContext));
+
+            _productRepository = new Lazy<IProductRepository>(() => new ProductRepository(_repositoryContext));
+
+            _commentRepository = new Lazy<ICommentRepository>(() => new CommentRepository(_repositoryContext));
+
+            _userRepository = new Lazy<IUserRepository>(() => new UserRepository(_repositoryContext));
+
+            _basketRepository = new Lazy<IBasketRepository>(() => new BaseketRepository(_repositoryContext)); 
+        }
+
+
+        public ITypeRepository Type => _typeRepository.Value;
+
+        public IProductRepository Repository => _productRepository.Value;
+
+        public ICommentRepository Comment => _commentRepository.Value;
+
+        public IUserRepository User => _userRepository.Value;
+
+        public IBasketRepository Basket => _basketRepository.Value; 
+
+        public async Task SaveAsync()
+        {
+            await _repositoryContext.SaveChangesAsync(); 
+        }
+    }
+}
---
+++ Repository.Contract/Abstractions/IRepositoryManager.cs
@@ -0,0 +1,13 @@
+﻿namespace Repository.Contract.Abstractions
+{
+    public interface IRepositoryManager
+    {
+        ITypeRepository Type { get; }
+        IProductRepository Repository { get;  }
+        ICommentRepository Comment { get; }
+        IUserRepository User { get; }
+        IBasketRepository Basket { get; }
+
+        Task SaveAsync(); 
+    }
+}
