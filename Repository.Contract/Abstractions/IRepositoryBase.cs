8131becec562fd96630740c13c6b3b964aecf0c5
Kerven Jumayev <kerwen.jumazew@gmail.com>
2025-03-24 17:33:04 +0300

    adding Repository Contracts repository

---
+++ Repository.Contract/Abstractions/IRepositoryBase.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq.Expressions;
+
+namespace Repository.Contract.Abstractions
+{
+    public interface IRepositoryBase<T>
+    {
+        IQueryable<T> GetAll(bool trackChanges);
+        IQueryable<T> FindByCondition(Expression<Func<T, bool>> exception, bool trackChanges);
+        Task CreateAsync(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
